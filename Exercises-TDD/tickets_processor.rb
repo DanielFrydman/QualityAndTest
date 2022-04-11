7555900389dee4a1dcbddca9ee47ca79f7d7ad8d
Daniel Frydman <danielfrydman@id.uff.br>
2022-04-11 20:06:26 -0300

    implement function

--- Exercises-TDD/tickets_processor.rb
+++ Exercises-TDD/tickets_processor.rb
@@ -1,4 +1,12 @@
 class TicketsProcessor
   def calculate(invoice:, tickets:)
+    sum = 0
+    payments = []
+    tickets.each do |ticket|
+      sum += ticket
+      payments << Payment.new(value: ticket, type: 'Boleto')
+    end
+
+    invoice.change_paid_status if invoice.value == sum
   end
 end
