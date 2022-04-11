1ab3b1e6dc93a3db9b8963278040500e23a7fff8
Daniel Frydman <danielfrydman@id.uff.br>
2022-04-11 20:22:04 -0300

    create second test senario

--- Exercises-TDD/spec/tickets_processor_spec.rb
+++ Exercises-TDD/spec/tickets_processor_spec.rb
@@ -4,10 +4,25 @@ require_relative '../payment.rb'
 
 RSpec.describe TicketsProcessor do
   describe '#calculate' do
-    context 'it receives the invoice and the tickets' do
+    context 'it receives the invoice(1500) and the tickets(500, 400, 600)' do
       let(:service) { TicketsProcessor.new }
       let(:invoice) { Invoice.new(1500) }
       let(:tickets) { [500, 400, 600] }
+
+      it 'returns invoice as paid and 3 payments of tickets created' do
+        response = service.calculate(invoice: invoice, tickets: tickets)
+        invoice = response[:invoice]
+        payments = response[:payments].count
+
+        expect(invoice.paid).to eq(true)
+        expect(payments).to eq(3)
+      end
+    end
+
+    context 'it receives the invoice(1500) and the tickets(1000, 500, 250)' do
+      let(:service) { TicketsProcessor.new }
+      let(:invoice) { Invoice.new(1500) }
+      let(:tickets) { [1000, 500, 250] }
 
       it 'returns invoice as paid and 3 payments of tickets created' do
         response = service.calculate(invoice: invoice, tickets: tickets)
