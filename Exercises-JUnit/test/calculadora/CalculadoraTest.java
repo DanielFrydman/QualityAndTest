18bd2bf6c021ae497361b2d41a10cd1acda91e4b
Daniel David Frydman <danielfrydman@id.uff.br>
2022-04-12 21:16:31 -0300

    added tests for calculator

--- Exercises-JUnit/test/calculadora/CalculadoraTest.java
+++ Exercises-JUnit/test/calculadora/CalculadoraTest.java
@@ -49,5 +49,52 @@ public class CalculadoraTest {
 		assertThrows(ArithmeticException.class,
 				() -> calc.divisao(8, 0));
 	}
+	
+	@Test
+	public void testSubtracaoDoisNumeros() {
+		int subtracao = calc.subtracao(5, 4);		
+		Assertions.assertEquals(1, subtracao);		
+	}
+	
+	@Test
+	public void testMultiplicacaoDoisNumeros() {
+		int multiplicacao = calc.multiplicacao(5, 4);		
+		Assertions.assertEquals(20, multiplicacao);		
+	}
+	
+	@Test
+	public void testSomatoria() {
+		int somatoria = calc.somatoria(5);		
+		Assertions.assertEquals(15, somatoria);		
+	}
+	
+	@Test
+	public void testEhPositivo1() {
+		boolean ehPositivo = calc.ehPositivo(5);		
+		Assertions.assertEquals(true, ehPositivo);
+	}
 
+	@Test
+	public void testEhPositivo2() {
+		boolean ehPositivo = calc.ehPositivo(-5);		
+		Assertions.assertEquals(false, ehPositivo);
+	}
+
+	@Test
+	public void testCompara1() {
+		int compara = calc.compara(5, 5);		
+		Assertions.assertEquals(0, compara);		
+	}
+	
+	@Test
+	public void testCompara2() {
+		int compara = calc.compara(5, 4);		
+		Assertions.assertEquals(1, compara);		
+	}
+	
+	@Test
+	public void testCompara3() {
+		int compara = calc.compara(4, 5);		
+		Assertions.assertEquals(-1, compara);		
+	}
 }
