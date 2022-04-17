8d3bd3a288d95441aee542870d1197e4bf79d3d8
Daniel David Frydman <danielfrydman@id.uff.br>
2022-04-17 19:15:24 -0300

    tdd pagamento class

--- Exercises-TDD-Java/src/pagamento/Pagamento.java
+++ Exercises-TDD-Java/src/pagamento/Pagamento.java
@@ -1,5 +1,18 @@
 package pagamento;
 
+import java.util.Date;
+
 public class Pagamento {
+	
+	private double valor;
+	private Date data;
+	private String tipo;
+	
+	public Pagamento(double valor, Date data, String tipo) {
+		super();
+		this.valor = valor;
+		this.data = data;
+		this.tipo = tipo;
+	}
 	
 }
