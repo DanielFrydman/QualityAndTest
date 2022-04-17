0628e42d8507c1b88ac7e7bc16acd349deafab2b
Daniel David Frydman <danielfrydman@id.uff.br>
2022-04-17 19:30:12 -0300

    added tests for fatura

--- Exercises-TDD-Java/src/fatura/Fatura.java
+++ Exercises-TDD-Java/src/fatura/Fatura.java
@@ -4,15 +4,15 @@ import java.util.Date;
 
 public class Fatura {
 	
-	private String nome_cliente;
-	private double valor_total;
+	private String nomeCliente;
+	private double valorTotal;
 	private Date data;
 	private boolean pago;
 	
-	public Fatura(String nome_cliente, double valor_total, Date data) {
+	public Fatura(String nomeCliente, double valorTotal, Date data) {
 		super();
-		this.nome_cliente = nome_cliente;
-		this.valor_total = valor_total;
+		this.nomeCliente = nomeCliente;
+		this.valorTotal = valorTotal;
 		this.data = data;
 		this.pago = false;
 	}
--- Exercises-TDD-Java/test/fatura/FaturaTest.java
+++ Exercises-TDD-Java/test/fatura/FaturaTest.java
@@ -2,7 +2,9 @@ package fatura;
 
 import java.util.Date;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class FaturaTest {
 	
@@ -11,6 +13,42 @@ public class FaturaTest {
 	@BeforeEach
 	public void inicializa() {
 		fatura = new Fatura("José", 1500.00, new Date());
+	}
+	
+	@Test
+	public void testGetNome() {
+		String nomeCliente = fatura.getNome();
+
+		Assertions.assertEquals(nomeCliente, "José");	
+	}
+	
+	@Test
+	public void testGetValorTotal() {
+		double valorTotal = fatura.getValorTotal();
+
+		Assertions.assertEquals(valorTotal, 1500.00);	
+	}
+	
+	@Test
+	public void testGetData() {
+		Date data = fatura.getData();
+
+		Assertions.assertEquals(data, new Date());	
+	}
+	
+	@Test
+	public void testGetPago() {
+		boolean pago = fatura.getPago();
+
+		Assertions.assertEquals(pago, false);	
+	}
+	
+	@Test
+	public void testSetPagoTrue() {
+		fatura.setPagoTrue();
+		boolean pago = fatura.getPago();
+
+		Assertions.assertEquals(pago, true);	
 	}
 
 }
