8cab6410f9e46f5056ac759e9704675d49c24afb
Daniel David Frydman <danielfrydman@id.uff.br>
2022-04-17 19:53:20 -0300

    beginning of boleto class

--- Exercises-TDD-Java/src/boleto/Boleto.java
+++ Exercises-TDD-Java/src/boleto/Boleto.java
@@ -1,5 +1,18 @@
 package boleto;
 
+import java.util.Date;
+
 public class Boleto {
+	
+	private int codigoDoBoleto;
+	private Date data;
+	private double valorTotal;
+	
+	public Boleto(int codigoDoBoleto, Date data, double valorTotal) {
+		super();
+		this.codigoDoBoleto = codigoDoBoleto;
+		this.data = data;
+		this.valorTotal = valorTotal;
+	}
 	
 }
