42c9071eb6a18e079d98a20ac6ae472a10c8f19b
Daniel David Frydman <danielfrydman@id.uff.br>
2022-04-17 19:52:29 -0300

    beginning of boleto test

--- Exercises-TDD-Java/test/boleto/BoletoTest.java
+++ Exercises-TDD-Java/test/boleto/BoletoTest.java
@@ -1,5 +1,16 @@
 package boleto;
 
+import java.util.Date;
+
+import org.junit.jupiter.api.BeforeEach;
+
 public class BoletoTest {
 	
+	Boleto boleto;
+	
+	@BeforeEach
+	public void inicializa() {
+		boleto = new Boleto(218083115, new Date(), 1500.00);
+	}
+
 }
