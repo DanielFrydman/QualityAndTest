68e2ea4c64a9fed0a9d76b40772d49373463a2f2
Daniel David Frydman <danielfrydman@id.uff.br>
2022-04-17 20:12:26 -0300

    beginning of processador de boletos class and refactor test

--- Exercises-TDD-Java/src/processador/ProcessadorDeBoletos.java
+++ Exercises-TDD-Java/src/processador/ProcessadorDeBoletos.java
@@ -1,5 +1,21 @@
 package processador;
 
+import java.util.ArrayList;
+
+import boleto.Boleto;
+import fatura.Fatura;
+import pagamento.Pagamento;
+
 public class ProcessadorDeBoletos {
+	
+	private ArrayList<Boleto> boletos;
+	private Fatura fatura;
+	private ArrayList<Pagamento> pagamentos;
+
+	public ProcessadorDeBoletos(ArrayList<Boleto> boletos, Fatura fatura, ArrayList<Pagamento> pagamentos) {
+		this.boletos = boletos;
+		this.fatura = fatura;
+		this.pagamentos = pagamentos;
+	}
  
 }
--- Exercises-TDD-Java/test/processador/ProcessadorDeBoletosTest.java
+++ Exercises-TDD-Java/test/processador/ProcessadorDeBoletosTest.java
@@ -1,5 +1,6 @@
 package processador;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -22,14 +23,12 @@ public class ProcessadorDeBoletosTest {
 		boleto1 = new Boleto(218083115, new Date(), 500.00);
 		boleto2 = new Boleto(218083116, new Date(), 400.00);
 		boleto3 = new Boleto(218083117, new Date(), 600.00);
-		Boleto[] arrayBoletos;
-		arrayBoletos = new Boleto[3];
-		arrayBoletos[0] = boleto1;
-		arrayBoletos[1] = boleto2;
-		arrayBoletos[2] = boleto3;
+		ArrayList<Boleto> arrayBoletos = new ArrayList<Boleto>();
+		arrayBoletos.add(boleto1);
+		arrayBoletos.add(boleto2);
+		arrayBoletos.add(boleto3);
 		fatura = new Fatura("José", 1500.00, new Date());
-		Pagamento[] arrayPagamentos;
-		arrayPagamentos = new Pagamento[3];
+		ArrayList<Pagamento> arrayPagamentos  = new ArrayList<Pagamento>();
 		
 		processador = new ProcessadorDeBoletos(arrayBoletos, fatura, arrayPagamentos);
 	}
