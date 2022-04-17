78841d9789b95f3d4b6d114420ac8679bba272a6
Daniel David Frydman <danielfrydman@id.uff.br>
2022-04-17 19:23:53 -0300

    beginning of fatura tests

--- Exercises-TDD-Java/test/fatura/FaturaTest.java
+++ Exercises-TDD-Java/test/fatura/FaturaTest.java
@@ -1,5 +1,16 @@
 package fatura;
 
+import java.util.Date;
+
+import org.junit.jupiter.api.BeforeEach;
+
 public class FaturaTest {
+	
+	Fatura fatura;
+	
+	@BeforeEach
+	public void inicializa() {
+		fatura = new Fatura("José", 1500.00, new Date());
+	}
 
 }
