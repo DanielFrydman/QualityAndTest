8b30aedf8cb668ba990f820a8f5aa6f9e0be4039
Daniel David Frydman <danielfrydman@id.uff.br>
2022-04-17 19:56:26 -0300

    added tests for boleto

--- Exercises-TDD-Java/test/boleto/BoletoTest.java
+++ Exercises-TDD-Java/test/boleto/BoletoTest.java
@@ -2,7 +2,9 @@ package boleto;
 
 import java.util.Date;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class BoletoTest {
 	
@@ -10,7 +12,28 @@ public class BoletoTest {
 	
 	@BeforeEach
 	public void inicializa() {
-		boleto = new Boleto(218083115, new Date(), 1500.00);
+		boleto = new Boleto(218083115, new Date(), 450.00);
+	}
+	
+	@Test
+	public void testGetCodigoDoBoleto() {
+		int codigoDoBoleto = boleto.getCodigoDoBoleto();
+
+		Assertions.assertEquals(codigoDoBoleto, 218083115);	
+	}
+	
+	@Test
+	public void testGetData() {
+		Date data = boleto.getData();
+
+		Assertions.assertEquals(data, new Date());	
+	}
+	
+	@Test
+	public void testGetValorTotal() {
+		double valorTotal = boleto.getValorTotal();
+
+		Assertions.assertEquals(valorTotal, 450.00);	
 	}
 
 }
