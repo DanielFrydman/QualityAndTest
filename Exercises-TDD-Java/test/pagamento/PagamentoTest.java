68b6c88271c35e4a4f12eba4c1ff4b8693210739
Daniel David Frydman <danielfrydman@id.uff.br>
2022-04-17 19:13:59 -0300

    beginning of pagamento test

--- Exercises-TDD-Java/test/pagamento/PagamentoTest.java
+++ Exercises-TDD-Java/test/pagamento/PagamentoTest.java
@@ -1,5 +1,16 @@
 package pagamento;
 
+import java.util.Date;
+
+import org.junit.jupiter.api.BeforeEach;
+
 public class PagamentoTest {
+	
+	Pagamento pagamento;
+	
+	@BeforeEach
+	public void inicializa() {
+		pagamento = new Pagamento(100.00, new Date(), "Boleto");
+	}
 	
 }
