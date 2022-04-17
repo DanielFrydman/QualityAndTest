b43a1ed1d993155462a021117fc85349cb0f09cd
Daniel David Frydman <danielfrydman@id.uff.br>
2022-04-17 19:24:25 -0300

    beginning of fatura class

--- Exercises-TDD-Java/src/fatura/Fatura.java
+++ Exercises-TDD-Java/src/fatura/Fatura.java
@@ -1,5 +1,20 @@
 package fatura;
 
+import java.util.Date;
+
 public class Fatura {
+	
+	private String nome_cliente;
+	private double valor_total;
+	private Date data;
+	private boolean pago;
+	
+	public Fatura(String nome_cliente, double valor_total, Date data) {
+		super();
+		this.nome_cliente = nome_cliente;
+		this.valor_total = valor_total;
+		this.data = data;
+		this.pago = false;
+	}
 	
 }
