9a7459218d51814c30b438591118ca8f56ad29e9
Daniel David Frydman <danielfrydman@id.uff.br>
2022-04-17 20:17:47 -0300

    refactor processador tests

--- Exercises-TDD-Java/test/processador/ProcessadorDeBoletosTest.java
+++ Exercises-TDD-Java/test/processador/ProcessadorDeBoletosTest.java
@@ -3,7 +3,9 @@ package processador;
 import java.util.ArrayList;
 import java.util.Date;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import boleto.Boleto;
 import fatura.Fatura;
@@ -12,25 +14,61 @@ import pagamento.Pagamento;
 public class ProcessadorDeBoletosTest {
 	
 	ProcessadorDeBoletos processador;
-	Boleto boleto1;
-	Boleto boleto2;
-	Boleto boleto3;
+	ArrayList<Boleto> arrayBoletos;
 	Fatura fatura;
-	Pagamento pagamento;
+	ArrayList<Pagamento> arrayPagamentos;
 	
 	@BeforeEach
 	public void inicializa() {
-		boleto1 = new Boleto(218083115, new Date(), 500.00);
-		boleto2 = new Boleto(218083116, new Date(), 400.00);
-		boleto3 = new Boleto(218083117, new Date(), 600.00);
-		ArrayList<Boleto> arrayBoletos = new ArrayList<Boleto>();
+		Boleto boleto1 = new Boleto(218083115, new Date(), 500.00);
+		Boleto boleto2 = new Boleto(218083116, new Date(), 400.00);
+		Boleto boleto3 = new Boleto(218083117, new Date(), 600.00);
+		arrayBoletos = new ArrayList<Boleto>();
 		arrayBoletos.add(boleto1);
 		arrayBoletos.add(boleto2);
 		arrayBoletos.add(boleto3);
 		fatura = new Fatura("José", 1500.00, new Date());
-		ArrayList<Pagamento> arrayPagamentos  = new ArrayList<Pagamento>();
+		arrayPagamentos = new ArrayList<Pagamento>();
 		
 		processador = new ProcessadorDeBoletos(arrayBoletos, fatura, arrayPagamentos);
 	}
 	
+	@Test
+	public void testGetListaBoletos() {
+		ArrayList<Boleto> array = processador.getListaBoletos();
+
+		Assertions.assertEquals(array, arrayBoletos);	
+	}
+	
+	@Test
+	public void testGetQuantidadeDeBoletos() {
+	}
+	
+	@Test
+	public void testGetQuantidadeDePagamentos() {
+	}
+	
+	@Test
+	public void testGetValorTotalBoletos() {
+	}
+	
+	@Test
+	public void testCriarPagamentos() {
+	}
+	
+	@Test
+	public void testAddPagamentoNaLista() {
+	}
+	
+	@Test
+	public void testChecarPagamento() {
+	}
+	
 }
+
+
+
+
+
+
+
